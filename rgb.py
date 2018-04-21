056ec0ff5b80d98645193a9299cb759864e55fb6
Brett.Vanderwerff <vande060@gmail.com>
2018-04-21 16:09:00 -0700

    edded error logger

--- rgb.py
+++ rgb.py
@@ -1,12 +1,17 @@
 from create_db import make_db
 from flask import Flask, render_template, url_for, redirect
 from forms import SubmitForm
+from logging import FileHandler, WARNING
 from pathlib import Path
 import sqlite3 as sql
+
+file_handler = FileHandler('errorlog.txt')
+file_handler.setLevel(WARNING)
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'your secret key goes here' #ToDo replace this with your secret key
+app.logger.addHandler(file_handler)
 
 @app.route('/')
 def index():
@@ -32,5 +37,5 @@ def view():
 if __name__ == '__main__':
     if not Path('database.db').is_file():
         make_db()
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0')
 
